f54995357c05cda235ff164d69b3a7f727949c66
AdeshSangale <sangaleadesh91@gmail.com>
2025-11-06 14:44:23 +0530

    Initial commit - portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // IMPORTANT: set your repo name here 👇
 export default defineConfig({
-  base: "adesh_portfolio/",
+  base: "/adeshportfolio/",
   plugins: [react()],
 });
